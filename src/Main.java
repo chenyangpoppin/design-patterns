4149da6e2e392a3b0f08aaa3b76bf4b02e493e78
Andy Chen <chenyangpoppin@hotmail.com>
2020-03-31 23:34:28 +0800

    Change comment thing for this project

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
-    
+
     // Entry point file for whole application
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Java Design Patterns");
 
     }
 
