baddda42c5a9016a1ff1a15b3e71bd123b6b8d6b
Andy Chen <chenyangpoppin@hotmail.com>
2020-04-08 08:16:44 +0800

    Add builder design pattern to the project

--- src/Builder/Student.java
+++ src/Builder/Student.java
@@ -2,9 +2,73 @@ package Builder;
 
 public class Student {
 
-    public void Show()
+    private final int m_studentNo;
+    private final int m_age;
+    private final String m_name;
+    private final String m_gender;
+
+    private Student(final Builder builder)
     {
-        System.out.println("Print out something to the console");
+        this.m_studentNo = builder.m_studentNo;
+        this.m_age = builder.m_age;
+        this.m_name = builder.m_name;
+        this.m_gender = builder.m_gender;
     }
 
+    public int getStudentNo()
+    {
+        return this.m_studentNo;
+    }
+
+    public int getAge()
+    {
+        return this.m_age;
+    }
+
+    public String getName()
+    {
+        return this.m_name;
+    }
+
+    public String getGender()
+    {
+        return this.m_gender;
+    }
+
+    public static class Builder
+    {
+        private int m_age;
+        private int m_studentNo;
+        private String m_name;
+        private String m_gender;
+
+        public Builder withStudentNo(int nStudentNo)
+        {
+            this.m_studentNo = nStudentNo;
+            return this;
+        }
+
+        public Builder withAge(int nAge)
+        {
+            this.m_age = nAge;
+            return this;
+        }
+
+        public Builder withName(String sName)
+        {
+            this.m_name = sName;
+            return this;
+        }
+
+        public Builder withGender(String sGender)
+        {
+            this.m_gender = sGender;
+            return this;
+        }
+
+        public Student build()
+        {
+            return new Student(this);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,14 @@ public class Main {
     // Entry point file for whole application
     public static void main(String[] args)
     {
-        Student stu = new Student();
-        stu.Show();
+        Student stu = new Student.Builder()
+                                 .withStudentNo(1)
+                                 .withAge(24)
+                                 .withName("Jackson")
+                                 .withGender("Female")
+                                 .build();
+
+        System.out.println(stu.getName());
     }
 
 }
